1215eb59e58bbdb84a6bcc652ec26665cad39b4a
ahallam <alexhallam75@gmail.com>
2023-02-03 15:48:44 -0500

    Home page setup, fetch setup, custom hook setup

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Header from './elements/Header';
+import Home from './Home';
+
+const App = () => (
+  <React.Fragment>
+    <Header />
+    <Home />
+  </React.Fragment>
+)
+
+export default App;
